28087aafe064f406f8fcf20463ae62e2d529a7ca
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-02-08 16:32:43 +0200

    Initial commit

---
+++ src/test/java/com/medilog/demo/MedilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilog.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
