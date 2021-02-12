3a685b57d6a844d2948b9c3cbebfd329239c21c8
Kristian Semjonov <kristian.semjonov@gmail.com>
2021-02-12 11:59:13 +0200

    reede 12.02

--- src/main/java/com/medilog/demo/medilog/TemperatureDiaryRowMapper.java
+++ src/main/java/com/medilog/demo/medilog/TemperatureDiaryRowMapper.java
@@ -4,10 +4,18 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class TemperatureDiaryRowMapper implements RowMapper<TemperatureDiary> {
     @Override
     public TemperatureDiary mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+        TemperatureDiary temperatureDiary = new TemperatureDiary();
+        temperatureDiary.setUserId(resultSet.getInt("user_id"));
+        temperatureDiary.setDate(resultSet.getObject("date", LocalDate.class));
+        temperatureDiary.setTime(resultSet.getObject("time", LocalTime.class));
+        temperatureDiary.setTemp(resultSet.getBigDecimal("temp"));
+        temperatureDiary.setAddInfo(resultSet.getString("add_info"));
+        return temperatureDiary;
     }
 }
