0e2870ff4d7357901b8b72e9112987634a66628e
Linda Ajaots <linda.ajaots@gmail.com>
2021-02-12 11:15:40 +0200

    bloodsugar ja bloodpressure eraldi htmlid

--- src/main/java/com/medilog/demo/medilog/BloodSugarDiaryRowMapper.java
+++ src/main/java/com/medilog/demo/medilog/BloodSugarDiaryRowMapper.java
@@ -4,10 +4,19 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class BloodSugarDiaryRowMapper implements RowMapper<BloodSugarDiary> {
     @Override
     public BloodSugarDiary mapRow(ResultSet resultSet, int i) throws SQLException {
-        return null;
+        BloodSugarDiary bloodSugarDiary = new BloodSugarDiary();
+        bloodSugarDiary.setUserId(resultSet.getInt("user_id"));
+        bloodSugarDiary.setDate(resultSet.getObject("date", LocalDate.class));
+        bloodSugarDiary.setTime(resultSet.getObject("time", LocalTime.class));
+        bloodSugarDiary.setBloodSugar(resultSet.getBigDecimal("blood_sugar"));
+        bloodSugarDiary.setAddInfo(resultSet.getString("add_info"));
+
+        return bloodSugarDiary;
     }
 }
